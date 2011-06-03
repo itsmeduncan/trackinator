601a0778534bcad9db4c7d20a5ed1dc3fa7e047b
Duncan Grazier <duncan@impossiblerocket.com>
2011-06-03 14:23:53 -0400

    fix migration

--- db/migrate/20110603174514_add_slug_to_victims.rb
+++ db/migrate/20110603174514_add_slug_to_victims.rb
@@ -3,13 +3,13 @@ class AddSlugToVictims < ActiveRecord::Migration
     add_column :victims, :slug, :string
 
     Victim.all.each do |victim|
-      victim.update_attribute(:slug, victim.to_param)
+      victim.update_attribute(:slug, victim.name.to_s.downcase.gsub(/[^A-Z0-9]/i, '-'))
     end
 
     add_index :victims, :slug, :unique => true
   end
 
   def self.down
-    remove_column :victims, :slug, :string, :null => false
+    remove_column :victims, :slug
   end
 end
